1e95111311875dc9d3c763a757c64cd99ea2f300
Marlen Brunner <klondikemarlen@gmail.com>
2020-11-15 12:07:15 -0700

    :truck: Move api test into an api folder.
    Also fix api/counties test.

---
+++ tests/api/test_users.py
@@ -0,0 +1,9 @@
+from expects import expect, have_keys
+
+class TestApiUsers:
+    def test_get_user(client):
+        """Start with a blank database."""
+
+        data = client.get('/api/users/1').get_json()
+
+        expect(data['user']).to(have_keys(id=1, username='test_user'))
