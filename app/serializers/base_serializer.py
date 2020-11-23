b495096061fc6bcea1d4d7bbc01e10824584ebf7
Marlen Brunner <klondikemarlen@gmail.com>
2020-11-22 17:34:21 -0700

    :pencil: Add documentation about camel case function.

--- app/serializers/base_serializer.py
+++ app/serializers/base_serializer.py
@@ -16,6 +16,18 @@ def fields(*names):
 
 
 def camel_case(string):
+    """Convert a string to camelCase.
+
+    Args:
+        string - string to be converted to camel case.
+
+    Examples:
+        camel_case("Hello World") == "helloWorld"
+        camel_case("Hello,world") == "hello,World"
+        camel_case("Hello_world") == "helloWorld"
+        camel_case("hello_world.txt_includehelp-WEBSITE") == \
+            "helloWorld.TxtIncludehelpWebsite"
+    """
     string = sub(r"(_|-)+", " ", string).title().replace(" ", "")
     return string[0].lower() + string[1:]
 
