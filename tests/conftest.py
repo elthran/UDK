a4f42ff250aa13ce741b7ac1385a31956decdfb7
Marlen Brunner <klondikemarlen@gmail.com>
2020-12-13 21:31:29 -0700

    :beetle::recycle: Re-suppress asyncio logging because it occurs when debugging.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,8 @@ import pytest
 
 from app.main import app
 
+asyncio_logger = logging.getLogger("asyncio")
+asyncio_logger.setLevel(logging.INFO)  # Silence useless asyncio debug messages.
 faker_logger = logging.getLogger("faker")
 faker_logger.setLevel(logging.INFO)  # Quiet faker locale messages down in tests.
 
